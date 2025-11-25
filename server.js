a459ae5ac5e7a8f54fee1323a36afd7631d0d603
AniketTancoder <154531133+AniketTancoder@users.noreply.github.com>
2025-11-25 09:49:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://precious-donut-fb5a07.netlify.app",
     methods: ["GET", "POST"]
   }
 });
